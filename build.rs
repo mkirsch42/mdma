79969f2304670e96b41df6c8e767f3447e03d078
Amber Kirschbaum <amber.fae.k@gmail.com>
2025-12-21 22:55:29 -0700

    print database url

--- build.rs
+++ build.rs
@@ -5,6 +5,10 @@ fn main() {
 }
 
 fn run_tailwind() {
+    println!(
+        "{}",
+        std::env::var("DATABASE_URL").unwrap_or("DATABASE_URL undefined".to_string())
+    );
     let result = Command::new("npx")
         .args(["tailwindcss", "-i", "styles.css", "-o", "static/styles.css"])
         .output();
